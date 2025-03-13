2c1924226bc2f1451009a6e1659b080ad1c6a2d4
Mohammed Hamza <muhammed7amza@gmail.com>
2025-03-13 03:50:28 +0200

    test

--- scripts/test.js
+++ scripts/test.js
@@ -1,1 +1,11 @@
-console.log('Hello, code review!');
+function updateClock() {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+  const timeString = `${hours}:${minutes}:${seconds}`;
+  console.log(timeString);
+}
+
+setInterval(updateClock, 1000);
+updateClock();
