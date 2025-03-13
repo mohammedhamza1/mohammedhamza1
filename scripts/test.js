bb480b157e3e719135e18d2bd77937a3101843cc
Mohammed Hamza <muhammed7amza@gmail.com>
2025-03-13 04:01:01 +0200

    test

--- scripts/test.js
+++ scripts/test.js
@@ -1,11 +1,54 @@
-function updateClock() {
-  const now = new Date();
-  const hours = String(now.getHours()).padStart(2, "0");
-  const minutes = String(now.getMinutes()).padStart(2, "0");
-  const seconds = String(now.getSeconds()).padStart(2, "0");
-  const timeString = `${hours}:${minutes}:${seconds}`;
-  console.log(timeString);
+import { FC } from "react";
+import { View, ViewStyle, TextStyle, ImageStyle } from "react-native";
+import { Icon, Text } from "@/components";
+import { useAppTheme } from "@/utils/useAppTheme";
+import { ThemedStyle } from "@/theme";
+import { isRTL, TxKeyPath } from "@/i18n";
+import { useNavigation } from "@react-navigation/native";
+
+interface CustomHeaderProps {
+  titleTx: TxKeyPath;
 }
-console.log(now);
-setInterval(updateClock, 1000);
-updateClock();
+
+export const CustomHeader: FC<CustomHeaderProps> = ({ titleTx }) => {
+  const {
+    themed,
+    theme: { colors },
+  } = useAppTheme();
+
+  const { goBack } = useNavigation();
+
+  return (
+    <View style={themed($customHeader)}>
+      <Icon
+        icon="arrowLeft"
+        style={themed($customHeaderIcon)}
+        size={32}
+        color={colors.palette.neutral900}
+        onPress={() => goBack()}
+      />
+      <Text style={themed($customHeaderTitle)} tx={titleTx} />
+    </View>
+  );
+};
+
+const $customHeader: ThemedStyle<ViewStyle> = ({ spacing }) => ({
+  flexDirection: "row",
+  alignItems: "center",
+  paddingTop: spacing.md,
+  paddingBottom: spacing.xs,
+});
+const $customHeaderIcon: ThemedStyle<ImageStyle> = ({ spacing }) => ({
+  transform: [{ rotate: isRTL ? "180deg" : "360deg" }],
+  marginRight: spacing.xs,
+});
+
+const $customHeaderTitle: ThemedStyle<TextStyle> = ({
+  colors,
+  typography,
+}) => ({
+  fontFamily: typography.fonts.somarSans.medium,
+  fontSize: 16,
+  fontWeight: 600,
+  color: colors.palette.neutral900,
+});
